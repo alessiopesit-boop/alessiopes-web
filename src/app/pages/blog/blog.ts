0d1504316451a13ccf443cf50627a568a37df9f5
Alessio Pes <alessio.pes.it@gmail.com>
2026-06-24 16:17:39 +0200

    feat(blog): toggle griglia/elenco nell'indice come in progetti

--- src/app/pages/blog/blog.ts
+++ src/app/pages/blog/blog.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, afterNextRender, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { RevealDirective } from '../../core/reveal.directive';
 import { ARTICLES, type Article } from '../../core/blog.data';
+
+type View = 'grid' | 'list';
+const STORAGE_KEY = 'ap-blog-view';
 
 @Component({
   selector: 'app-blog',
@@ -10,4 +13,24 @@ import { ARTICLES, type Article } from '../../core/blog.data';
 })
 export class Blog {
   readonly articles: Article[] = ARTICLES;
+  readonly view = signal<View>('grid');
+
+  constructor() {
+    afterNextRender(() => {
+      try {
+        if ((localStorage.getItem(STORAGE_KEY) as View) === 'list') this.view.set('list');
+      } catch {
+        /* ignore */
+      }
+    });
+  }
+
+  setView(v: View): void {
+    this.view.set(v);
+    try {
+      localStorage.setItem(STORAGE_KEY, v);
+    } catch {
+      /* ignore */
+    }
+  }
 }
