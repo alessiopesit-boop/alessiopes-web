4ffde8b5ddb19ac5cf30fbc92fbb079a3739898d
Alessio Pes <alessio.pes.it@gmail.com>
2026-09-22 16:44:05 +0200

    feat(chi-sono): foto profilo (headshot) nella pagina Chi sono

--- src/app/pages/chi-sono/chi-sono.ts
+++ src/app/pages/chi-sono/chi-sono.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { ImageSlot } from '../../shared/image-slot/image-slot';
 import { RevealDirective } from '../../core/reveal.directive';
 
 @Component({
   selector: 'app-chi-sono',
-  imports: [RouterLink, ImageSlot, RevealDirective],
+  imports: [RouterLink, RevealDirective],
   templateUrl: './chi-sono.html',
 })
 export class ChiSono {}
